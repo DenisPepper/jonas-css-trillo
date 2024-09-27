8f8b0a83ecda3e738705bfaa01092200560ed2a6
DenisPepper <thech@yandex.ru>
2024-09-27 18:47:06 +0300

    Добавит основные разделы и стили

---
+++ src/componets/app/app.jsx
@@ -0,0 +1,13 @@
+import './app.scss';
+
+export function App() {
+  return (
+    <div className='container'>
+      <header className='header'>Header</header>
+      <div className='content'>
+        <nav className='sidebar'>Sidebar</nav>
+        <main className='hotel-view'>Content</main>
+      </div>
+    </div>
+  );
+}
